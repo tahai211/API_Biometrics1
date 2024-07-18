45aaed2cf66fce28e6787f6e9f36b1ab1db88dd8
tahai211 <taiich197@gmail.com>
2024-07-18 23:26:55 +0700

    commit

---
+++ MockUp_CardZ/DTO/Entity/EncryptionType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockUp_CardZ.DTO.Entity
+{
+    public class EncryptionType
+    {
+    }
+}
